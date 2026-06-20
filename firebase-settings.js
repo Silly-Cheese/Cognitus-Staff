2b07e6af02b58fbab9ea6207a075e855b7d44117
Executive_Eagle <christophershelley257@gmail.com>
2026-06-20 09:45:28 -0500

    Refactor firebase-settings.js to simplify config
    Removed unnecessary script tags and initialization code.

--- firebase-settings.js
+++ firebase-settings.js
@@ -1,11 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBnO_w_n-80sICCYtUreZy2OMQ-LtMDTJ8",
     authDomain: "cognitus-staff.firebaseapp.com",
     projectId: "cognitus-staff",
@@ -13,7 +6,3 @@
     messagingSenderId: "908912899910",
     appId: "1:908912899910:web:14c7f000933634c079d747"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
