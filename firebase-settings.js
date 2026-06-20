1a28a11bd2becb63a7a257f1b03e7a01c40b63f7
Executive_Eagle <christophershelley257@gmail.com>
2026-06-20 09:45:55 -0500

    Fix Firebase settings export

--- firebase-settings.js
+++ firebase-settings.js
@@ -1,8 +1,8 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBnO_w_n-80sICCYtUreZy2OMQ-LtMDTJ8",
-    authDomain: "cognitus-staff.firebaseapp.com",
-    projectId: "cognitus-staff",
-    storageBucket: "cognitus-staff.firebasestorage.app",
-    messagingSenderId: "908912899910",
-    appId: "1:908912899910:web:14c7f000933634c079d747"
-  };
+export const firebaseSettings = {
+  apiKey: "AIzaSyBnO_w_n-80sICCYtUreZy2OMQ-LtMDTJ8",
+  authDomain: "cognitus-staff.firebaseapp.com",
+  projectId: "cognitus-staff",
+  storageBucket: "cognitus-staff.firebasestorage.app",
+  messagingSenderId: "908912899910",
+  appId: "1:908912899910:web:14c7f000933634c079d747"
+};
