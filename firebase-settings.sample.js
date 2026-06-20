2d2cdf509a9cf6547a1743e006e37e6e60c1c924
Executive_Eagle <christophershelley257@gmail.com>
2026-06-20 09:36:21 -0500

    Add Firebase settings sample

---
+++ firebase-settings.sample.js
@@ -0,0 +1,6 @@
+// Rename this file to firebase-settings.js.
+// Paste the web app settings object from your Firebase project below.
+
+export const firebaseSettings = {
+  // paste Firebase web app settings here
+};
